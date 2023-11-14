14d4bdb3ff08c7d6fa0179598c3c2572792f03a1
carol <carolina.blanco.7e6@itb.cat>
2023-11-14 19:46:45 +0100

    second commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+<h1>All destinations</h1>
+
+<section class="destinations-box">
+  <article class="destination-box">
+    <h2>Title</h2>
+    <img class="destination-cover" src="https://as01.epimg.net/diarioas/imagenes/2022/04/20/actualidad/1650466413_240889_1650466661_noticia_normal_recorte1.jpg" alt="" srcset="">
+  </article>
+  <article class="destination-box"></article>
+  <article class="destination-box"></article>
+  <article class="destination-box"></article>
+</section>
   </main>
 </template>
+
+<style scoped>
+
+h1,h2{
+  font-weight: bold;
+}
+
+.destinations-box{
+  display: flex;
+}
+
+.destination-cover{
+  width: 200px;
+  height: 125px;
+}
+
+
+</style>
